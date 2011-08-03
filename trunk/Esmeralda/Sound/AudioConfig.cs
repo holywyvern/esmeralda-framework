e1b105b663289d12c76462f836fd44ab7e60043c
ramiro.rojo.cretta <ramiro.rojo.cretta@8f4255a1-8661-bc51-adb2-0c41d48c3579>
2011-08-03 04:06:17 +0000

    Added The audio module.

---
+++ trunk/Esmeralda/Sound/AudioConfig.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+
+namespace Esmeralda.Sound
+{
+    public class AudioConfig
+    {
+
+        #region Variable Declaration
+        private ContentManager contentManager;
+        private Dictionary<string, string> paths;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// AudioConfig sets the game audio locations
+        /// </summary>
+        /// <param name="contentManager">the content manager</param>
+        public AudioConfig(ContentManager contentManager)
+        {
+            this.contentManager = contentManager;
+            this.paths = new Dictionary<string, string>();
+            this.paths.Add("BGM", "");
+            this.paths.Add("BGS", "");
+            this.paths.Add("ME", "");
+            this.paths.Add("SE", "");
+        }
+        #endregion
+
+        #region Set Path Methods
+        /// <summary>
+        /// Sets the audio to find Background Musics files here
+        /// </summary>
+        /// <param name="dir">the location of the directory</param>
+        public void SetBgmDir(string dir)
+        {
+            this.paths["BGM"] = dir;
+        }
+
+        /// <summary>
+        /// Sets the audio to find Back Ground Sounds files here
+        /// </summary>
+        /// <param name="dir">the location of the directory</param>
+        public void SetBgsDir(string dir)
+        {
+            this.paths["BGS"] = dir;
+        }
+
+        /// <summary>
+        /// Sets the audio to find Musical Effects files here
+        /// </summary>
+        /// <param name="dir">the location of the directory</param>
+        public void SetMeDir(string dir)
+        {
+            this.paths["Me"] = dir;
+        }
+
+        /// <summary>
+        /// Sets the audio to find Sound effects files here
+        /// </summary>
+        /// <param name="dir">the location of the directory</param>
+        public void SetSeDir(string dir)
+        {
+            this.paths["Se"] = dir;
+        }
+        #endregion
+
+        #region Get content Manager
+        /// <summary>
+        /// Gets the Content Manager
+        /// </summary>
+        public ContentManager ContentManager
+        {
+            get { return this.contentManager; }
+        }
+        #endregion
+
+        #region Get Paths
+        /// <summary>
+        /// Gets the paths of audio data
+        /// </summary>
+        public Dictionary<string, string> Paths
+        {
+            get { return this.paths; }
+        }
+        #endregion
+    }
+}
---
+++ trunk/Esmeralda/Sound/AudioFile.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esmeralda.Sound
+{
+    public class AudioFile
+    {
+        #region Variable Declaration
+        private int volume;
+        private int pitch;
+        private int pan;
+        private string name;
+        #endregion
+
+        #region Volume
+        /// <summary>
+        /// Sets or Gets the volume of the audio from 
+        /// 0 meaning mute to 100 meaning max volume
+        /// </summary>
+        public int Volume
+        {
+            get { return this.volume; }
+            set { this.volume = (value > 100) ? 100 : ((value < 0) ? 0 : value); }
+        }
+        #endregion
+
+        #region Pitch
+        /// <summary>
+        /// Sets the pitch of the File from the lowest 50 to the highest 150. 100 means normal.
+        /// </summary>
+        public int Pitch
+        {
+            get { return this.pitch; }
+            set { this.pitch = (value > 150) ? 100 : ((value < 50) ? 50 : value); }
+        }
+        #endregion
+
+        #region Pan
+        /// <summary>
+        /// Sets the balance of the speakers. from -100 meaning left balance to 100 meaning right balance.
+        /// 0 means center.
+        /// </summary>
+        public int Pan
+        {
+            get { return this.pan; }
+            set { this.pan = (value < -100) ? -100 : ( (value > 100) ? 100 : value);}
+        }
+        #endregion
+
+        #region Name
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new Audio file to play
+        /// </summary>
+        /// <param name="name">the name of the file</param>
+        public AudioFile(string name)
+        {
+            this.Name = name;
+            this.Volume = 100;
+            this.Pitch = 100; 
+        }
+
+        /// <summary>
+        /// Creates a new Audio file to play. 
+        /// </summary>
+        /// <param name="name">the name of the file</param>
+        /// <param name="volume">the volume of the file in the range of 0 to 100</param>
+        public AudioFile(string name, int volume)
+        {
+            this.Name = name;
+            this.Volume = volume;
+            this.Pitch = 100;
+            this.Pan = 0;
+        }
+
+        /// <summary>
+        /// Creates a new Audio file to play
+        /// </summary>
+        /// <param name="name">the name of the file</param>
+        /// <param name="volume">the volume of the file in the range of 0 to 100</param>
+        /// <param name="pitch">the pitch of the file in the range of 50 to 150</param>
+        public AudioFile(string name, int volume, int pitch)
+        {
+            this.Name = name;
+            this.Volume = volume;
+            this.Pitch = pitch;
+            this.Pan = 0;
+        }
+
+        /// <summary>
+        /// Creates a new Audio file to play
+        /// </summary>
+        /// <param name="name">the name of the file</param>
+        /// <param name="volume">the volume of the file in the range of 0 to 100</param>
+        /// <param name="pitch">the pitch of the file in the range of 50 to 150</param>
+        /// <param name="pan">the pan of the file in the range of -100 to 100</param>
+        public AudioFile(string name, int volume, int pitch, int pan)
+        {
+            this.Name = name;
+            this.Volume = volume;
+            this.Pitch = pitch;
+            this.Pan = pan;
+        }
+        #endregion
+    }
+}
